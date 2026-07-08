0f7b7ad2049acec7d906ffb8cb27437bbc33ca83
Teng Lee <tenglee4@gmail.com>
2026-07-07 20:54:42 -0500

    Add isolated database subnets

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -27,3 +27,8 @@ output "private_app_subnet_ids" {
   description = "IDs of the private application subnets."
   value       = aws_subnet.private_app[*].id
 }
+
+output "isolated_db_subnet_ids" {
+  description = "IDs of the isolated database subnets."
+  value       = aws_subnet.isolated_db[*].id
+}
--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -27,3 +27,18 @@ resource "aws_subnet" "private_app" {
     Tier    = "private-app"
   }
 }
+
+resource "aws_subnet" "isolated_db" {
+  count = 2
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.isolated_db_subnet_cidrs[count.index]
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "${var.project_name}-isolated-db-${count.index + 1}"
+    Project = var.project_name
+    Tier    = "isolated-db"
+  }
+}
